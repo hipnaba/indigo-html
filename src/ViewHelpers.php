7a806e97bef0c9a2b8fed4ae56e2f73d4862ccab
hipnaba <hipnaba@gmail.com>
2017-02-27 01:43:23 +0100

    updated helper phpdoc

--- src/ViewHelpers.php
+++ src/ViewHelpers.php
@@ -1,16 +1,16 @@
 <?php
 namespace Indigo\Html;
 
-use Indigo\View\ViewHelpers as IndigoViewHelpers;
-
 /**
  * Interface ViewHelpers
  *
  * @package Indigo\Html
  * @author  Danijel Fabijan <hipnaba@gmail.com>
  * @link    https://github.com/hipnaba/indigo-html
+ *
+ * @method string|Helper\HtmlElement htmlElement(ElementInterface $element = null)
  */
-interface ViewHelpers extends IndigoViewHelpers
+interface ViewHelpers
 {
 
 }
